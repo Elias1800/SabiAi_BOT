951dab2aac8dee0cfc2ad0cf7628b13986d1d2cf
Elias Barbosa Teixeira <54732626+Elias1800@users.noreply.github.com>
2025-05-21 23:02:01 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SabiAi_BOT
+
---
+++ teste_conexao.py
@@ -0,0 +1,24 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+# APIs
+TOKEN_TELEGRAM= '7654266195:AAGYNmiIglcTHP_prxkf4VL46s3QqQLIOm0'
+TOKENN_GEMINI=''
+
+# Função que responde ao comando /start
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f"Olá, {update.effective_user.first_name}! Eu estou funcionando! 🤖")
+
+async def Eu(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("I LOVE YOU!<3")
+
+
+# Inicializa o bot
+if __name__ == '__main__':
+    app = ApplicationBuilder().token(TOKEN_TELEGRAM).build()
+
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("Eu", Eu))
+
+    print("Bot iniciado...")
+    app.run_polling()
